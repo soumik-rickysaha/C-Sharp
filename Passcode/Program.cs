b44b2eb727dbc271a9ac14fd8ec89732ca17e29f
Soumik Saha <soumiksaha.92@hotmail.com>
2024-07-06 02:13:29 +0530

    Passcode

--- Passcode/Program.cs
+++ Passcode/Program.cs
@@ -1,1 +1,22 @@
 ﻿
+
+using System.Runtime.Intrinsics.X86;
+
+class Passcode
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Emter the passcode :");
+        String? passcode=Console.ReadLine(); //To fix this warning, you can use the null - forgiving operator (?) to tell the compiler that you are sure the value will not be null
+
+        if (passcode == "secret")
+        {
+            Console.WriteLine("Pass");
+        }
+        else
+        {
+            Console.WriteLine("Fail");
+        }
+
+    }
+}
