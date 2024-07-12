ac9dfb151fa70c521ce094e62911ed6aa3efba0f
Soumik Saha <soumiksaha.92@hotmail.com>
2024-07-12 19:56:11 +0530

    Library Management Initial Commit

---
+++ Library-Management-System/Menu/LibraryMenu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System.Menu
+{
+    internal class LibraryMenu
+    {
+       public void LoadBookMenu()
+        {
+            Console.WriteLine("1. Add Book");
+            Console.WriteLine("2. Delete Book");
+            Console.WriteLine("3. Update Book");
+            Console.WriteLine("4. Return Book");
+        }
+
+        public void LoadAdminMenu()
+        {
+            Console.WriteLine("1. Login");            
+            Console.WriteLine("2. Add Admin");
+            Console.WriteLine("3. Create Student Card");
+            Console.WriteLine("4. Print Book Inventory Count");
+            Console.WriteLine("5. Print Total Student Cards");
+            Console.WriteLine("6. Add Librarian");
+            Console.WriteLine("7. Update Librarian Details");
+            Console.WriteLine("8. Delete Librarian");
+        }
+    }
+}
